29122442b04d7a1b8882706d278df05e9404750c
Ram Kinkar Pandey <ram.b.pandey@avanade.com>
2023-03-21 20:54:42 +0000

    fixed setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
-from e2e_mlops_ado import __version__
+from telco_churn import __version__
 
 setup(
-    name="e2e_mlops_ado",
+    name="telco_churn",
     packages=find_packages(exclude=["tests", "tests.*"]),
     setup_requires=["wheel"],
     version=__version__,
